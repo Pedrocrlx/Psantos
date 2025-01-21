d6853ec99baf39c4b206be2d36496bcb2c8c195f
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-01-21 21:58:08 +0000

    wyp 97% done

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,13 @@ async function loadFooter() {
     document.getElementById("footer").innerHTML = navbarHtml;
 }
 
+function socialGitHub() {
+    window.location.href = "https://github.com/Pedrocrlx";  // URL de destino
+}
+
+function socialLinkedin() {
+    window.location.href = "https://linkedin.com";  // URL de destino
+}
+
 loadFooter();
 loadNavbar();
