08196a3af00d7218389526e63dcc96a479d0064a
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-01-23 21:50:37 +0000

    little adjust

--- scripts/main.js
+++ scripts/main.js
@@ -30,12 +30,12 @@ form.addEventListener("submit", (event) => {
     const number = document.getElementById('number').value;
     if (!email.includes("@")) {
         event.preventDefault();
-        erro.innerText = "Introduza um e-mail válido! '@ e .' ";
+        erro.innerText = "Introduza um e-mail válido! '@' ";
     }
      else if (!name.trim()) {
         event.preventDefault();
         erro.innerText = "O campo 'Empresa' é obrigatório! ";
-    } else if (!number > 0 && !number < 9) {
+    } else if (!number > 0) {
         event.preventDefault();
         erro.innerText = "Introduza um número, e que tenha 9 digitos! ";
     } else {
