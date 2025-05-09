c15f4240115aa598753e02333e1082a54800ab49
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-05-09 19:43:58 +0100

    projects page

---
+++ scripts/links.js
@@ -0,0 +1,15 @@
+function socialGitHub() {
+    window.location.href = "https://github.com/Pedrocrlx";
+}
+
+function socialLinkedin() {
+    window.location.href = "https://linkedin.com";
+}
+
+function frontEnd1Aulas() {
+    window.location.href = "https://pedrocrlx.github.io/frontend1-aulas/";
+}
+
+function frontEnd1Projecto() {
+    window.location.href = "https://pedrocrlx.github.io/frontend1-projeto/";
+}
--- scripts/main.js
+++ scripts/main.js
@@ -10,35 +10,5 @@ async function loadFooter() {
     document.getElementById("footer").innerHTML = navbarHtml;
 }
 
-function socialGitHub() {
-    window.location.href = "https://github.com/Pedrocrlx";  // URL de destino
-}
-
-function socialLinkedin() {
-    window.location.href = "https://linkedin.com";  // URL de destino
-}
-
 loadFooter();
 loadNavbar();
-
-const form = document.getElementById("formValidation");
-const erro = document.getElementById("erro");
-
-form.addEventListener("submit", (event) => {
-    const name = document.getElementById("company").value;
-    const email = document.getElementById("email").value;
-    const number = document.getElementById('number').value;
-    if (!email.includes("@")) {
-        event.preventDefault();
-        erro.innerText = "Introduza um e-mail válido! '@' ";
-    }
-     else if (!name.trim()) {
-        event.preventDefault();
-        erro.innerText = "O campo 'Empresa' é obrigatório! ";
-    } else if (!number > 0) {
-        event.preventDefault();
-        erro.innerText = "Introduza um número, e que tenha 9 digitos! ";
-    } else {
-        erro.innerText = "";
-    }
-})
